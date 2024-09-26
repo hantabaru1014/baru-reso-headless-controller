f72d4d6efec1dc7ea65953f33ff39b0473007fca
hantabaru1014 <hantabaru1014@gmail.com>
2024-09-26 16:12:12 +0000

    add sveltestrap

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
-  import { createPromiseClient } from "@connectrpc/connect";
-  import { createConnectTransport } from "@connectrpc/connect-web";
-  import { UserService } from "$lib/pbgen/hdlctrl/v1/user_connect";
-	import { ControllerService } from "$lib/pbgen/hdlctrl/v1/controller_connect";
-	import type { HeadlessHost } from "$lib/pbgen/hdlctrl/v1/controller_pb";
+	import { createPromiseClient } from '@connectrpc/connect';
+	import { createConnectTransport } from '@connectrpc/connect-web';
+	import { UserService } from '$lib/pbgen/hdlctrl/v1/user_connect';
+	import { ControllerService } from '$lib/pbgen/hdlctrl/v1/controller_connect';
+	import type { HeadlessHost } from '$lib/pbgen/hdlctrl/v1/controller_pb';
 
-  const transport = createConnectTransport({
-    baseUrl: '/',
-  });
-  const userServiceClient = createPromiseClient(UserService, transport);
-  const ctrlServiceClient = createPromiseClient(ControllerService, transport);
+	const transport = createConnectTransport({
+		baseUrl: '/'
+	});
+	const userServiceClient = createPromiseClient(UserService, transport);
+	const ctrlServiceClient = createPromiseClient(ControllerService, transport);
 
-  let apiKey = '';
-  let hosts: HeadlessHost[] = [];
+	let apiKey = '';
+	let hosts: HeadlessHost[] = [];
 
-  const fetchToken = async () => {
-    const res = await userServiceClient.getTokenByAPIKey({
-      apiKey,
-    });
+	const fetchToken = async () => {
+		const res = await userServiceClient.getTokenByAPIKey({
+			apiKey
+		});
 
-    return res.token;
-  };
+		return res.token;
+	};
 
-  const fetchHosts = async () => {
-    const token = await fetchToken();
-    const res = await ctrlServiceClient.listHeadlessHost({}, {
-      headers: [
-        ['authorization', `Bearer ${token}`]
-      ]
-    });
+	const fetchHosts = async () => {
+		const token = await fetchToken();
+		const res = await ctrlServiceClient.listHeadlessHost(
+			{},
+			{
+				headers: [['authorization', `Bearer ${token}`]]
+			}
+		);
 
-    hosts = res.hosts;
-  };
+		hosts = res.hosts;
+	};
 </script>
 
 <h1>Welcome to baru-reso-headless</h1>
@@ -44,7 +45,13 @@
 <h2>Hosts</h2>
 
 <ul>
-  {#each hosts as host}
-    <li>{host.id} : {host.name}</li>
-  {/each}
+	{#each hosts as host}
+		<li>{host.id} : {host.name}</li>
+	{/each}
 </ul>
+
+{#each new Array(20).map((_, i) => i) as i}
+	<div class="mt-5">
+		クソ長コンテンツ {i}
+	</div>
+{/each}
