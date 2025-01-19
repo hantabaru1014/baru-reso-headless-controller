fb9c1bc81894cef3d884b8692bdca515f2858048
hantabaru1014 <hantabaru1014@gmail.com>
2025-01-20 00:45:48 +0900

    add reload btn

--- front/src/components/HostList.tsx
+++ front/src/components/HostList.tsx
@@ -2,7 +2,9 @@ import { useQuery } from "@connectrpc/connect-query";
 import { listHeadlessHost } from "../../pbgen/hdlctrl/v1/controller-ControllerService_connectquery";
 import {
   Grid2,
+  IconButton,
   Skeleton,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -11,51 +13,61 @@ import {
   TableRow,
 } from "@mui/material";
 import prettyBytes from "../libs/prettyBytes";
+import { Refresh } from "@mui/icons-material";
 
 export default function HostList() {
-  const { data, isPending } = useQuery(listHeadlessHost);
+  const { data, isPending, refetch } = useQuery(listHeadlessHost);
 
   return (
-    <Grid2 container spacing={2}>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>名前</TableCell>
-              <TableCell>Resoniteバージョン</TableCell>
-              <TableCell>fps</TableCell>
-              <TableCell>アカウント名</TableCell>
-              <TableCell>ストレージ</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data?.hosts.map((host) => (
-              <TableRow key={host.id}>
-                <TableCell>{host.id}</TableCell>
-                <TableCell>{host.name}</TableCell>
-                <TableCell>{host.resoniteVersion}</TableCell>
-                <TableCell>{host.fps}</TableCell>
-                <TableCell>{host.accountName}</TableCell>
-                <TableCell>{`${prettyBytes(Number(host.storageUsedBytes))}/${prettyBytes(Number(host.storageQuotaBytes))}`}</TableCell>
+    <Grid2 container>
+      <Grid2 size={12}>
+        <Stack direction="row" spacing={2} sx={{ justifyContent: "flex-end" }}>
+          <IconButton aria-label="再読み込み" onClick={() => refetch()}>
+            <Refresh />
+          </IconButton>
+        </Stack>
+      </Grid2>
+      <Grid2 size={12}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>名前</TableCell>
+                <TableCell>Resoniteバージョン</TableCell>
+                <TableCell>fps</TableCell>
+                <TableCell>アカウント名</TableCell>
+                <TableCell>ストレージ</TableCell>
               </TableRow>
-            ))}
-            {isPending && (
-              <>
-                <TableRow>
-                  <Skeleton variant="rectangular" />
+            </TableHead>
+            <TableBody>
+              {data?.hosts.map((host) => (
+                <TableRow key={host.id}>
+                  <TableCell>{host.id}</TableCell>
+                  <TableCell>{host.name}</TableCell>
+                  <TableCell>{host.resoniteVersion}</TableCell>
+                  <TableCell>{host.fps}</TableCell>
+                  <TableCell>{host.accountName}</TableCell>
+                  <TableCell>{`${prettyBytes(Number(host.storageUsedBytes))}/${prettyBytes(Number(host.storageQuotaBytes))}`}</TableCell>
                 </TableRow>
-                <TableRow>
-                  <Skeleton variant="rectangular" />
-                </TableRow>
-                <TableRow>
-                  <Skeleton variant="rectangular" />
-                </TableRow>
-              </>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              ))}
+              {isPending && (
+                <>
+                  <TableRow>
+                    <Skeleton variant="rectangular" />
+                  </TableRow>
+                  <TableRow>
+                    <Skeleton variant="rectangular" />
+                  </TableRow>
+                  <TableRow>
+                    <Skeleton variant="rectangular" />
+                  </TableRow>
+                </>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid2>
     </Grid2>
   );
 }
