a9a6c0a7df2d47890edfbf2d8f197c060c033e8b
hantabaru1014 <hantabaru1014@gmail.com>
2025-06-17 02:59:07 +0900

    storybookを導入

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../front/src/**/*.mdx",
+    "../front/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
